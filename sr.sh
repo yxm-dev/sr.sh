b8742d34bdd6ef2fd5ca48e3627505c35f938726
yxm <yxm.dev@gmx.ie>
2024-01-13 17:42:37 -0300

    ...

--- sr.sh
+++ sr.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-function sr(){
+function sr(){   
     if [[ ! -d "$3" ]]; then
         echo "error: target directory not found."
         return 1
@@ -12,28 +12,38 @@ function sr(){
             declare -a line_numbers
             declare -a line_contents
 
+            red="\e[31m"
+            green="\e[32m"
+            blue="\e[34m"
+            magenta="\e[35m"
+            default="\e[0m"
+
             while IFS=: read -r filename line_number line_content
             do
                 text_occurrences+=("$1")
-                file_names+=("$filename")
-                line_numbers+=("$line_number")
+                file_names+=("${filename}")
+                line_numbers+=($line_number)
                 content=$(echo $line_content | sed 's/[^:]*://' )
+                content=$(echo $content | sed "s/$1/\\${magenta}$1\\${default}/")
                 line_contents+=("$content")
-            done < <(rg --vimgrep --color=always --colors 'match:bg:white' "$1" "$3")
+            done < <(rg --vimgrep "$1" "$3")
 
             if [[ -z "${text_occurrences[@]}" ]]; then
                 echo "No ocurrences of \"$1\" were found in \"$3\"."
                 return 0
             else
                 for i in "${!text_occurrences[@]}"; do
-                    echo "Ocurrency \"$((i+1))\" in file \"${file_names[$i]}\": "
-                    echo -e "${line_numbers[$i]}: ${line_contents[$i]}"
-                    echo "Replace \"$1\" with \"$2\"? (y/n)"
+                    echo -e "Ocurrency \"${red}$((i+1))${default}\" in file \"${blue}${file_names[$i]}${default}\": "
+                    echo ""
+                    echo -e "${green}${line_numbers[$i]}${default}: ${line_contents[$i]}"
+                    echo ""
+                    echo -e "Replace \"${magenta}$1${default}\" with \"$2\"? (y/n)"
                     while :
                     do
                         read -r -p "> " replace
                         if [[ "$replace" == "yes" ]] || [[ "$replace" == "y" ]]; then
-                            sed -i "${line_numbers[$i]}s/$1/$2/" ${file_names[$i]}
+                            sed -i "${line_numbers[$i]}s/$1/$2/" ${file_names[$i]} 
+
                             echo "Done!"
                             break
                         elif [[ "$replace" == "no" ]] || [[ "$replace" == "n" ]]; then
